e47ef0b7fc60ee046221fb5fa74b130120ed18c0
Conteyner <conteynerbich@gmail.com>
2023-05-05 10:46:13 +0300

    много крутого

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-  // функция для скроллинга страницы вверх
-  function topFunction() {
-    document.body.scrollTop = 0; // для Safari
-    document.documentElement.scrollTop = 0; // для других браузеров
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
 }
 
-// показываем кнопку, когда пользователь прокрутил вниз на 20px от верхней части страницы
 window.onscroll = function() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("scroll-to-top").style.display = "block";
